ac5b634911941fb7d38a7288d6d8e31e94f1d130
gqbeerman <gqbeerman@yahoo.com>
2020-11-15 20:57:10 -0800

    adding latest files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  render(); {
+      return (
+        <div className="App">
+          <Particles className='particles' params={particleOptions}/>
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+          {
+            // <FaceRecognition />
+          }
+        </div>
+    );
+  }
 }
 
 export default App;
